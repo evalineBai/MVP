98de57055562586ab6494a86c17afea9006a0737
Evaline <evalinebai@gmail.com>
2018-02-17 13:35:17 -0800

    Build server functionality

---
+++ server/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('../database/index.js');
+const request = require('request');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client/dist'));
+
+
+app.post('/accounts', function(req, res) {
+  let address = req.body.address;
+
+  let options = {
+    url: `https://api.blockcypher.com/v1/btc/main/addrs/${address}`,
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'User-Agent': 'request'
+    }
+  };
+  request(options, function(err, res, body) {
+    if (err) {
+      console.log(err);
+    }
+
+    let data = JSON.parse(body);
+    if (data.txrefs === undefined) {
+      let transactions = [{
+        hash: 'unavailable',
+        amount: 0,
+        date: 'unavailable'
+      }]
+    } else {
+      transactions = data.txrefs.map((trans) => {
+        return {
+          hash: trans.tx_hash,
+          amount: trans.value,
+          date: trans.confirmed
+        }
+      });
+    }
+    let blockData = {
+      address: data.address,
+      balance: data.balance,
+      sent: data.total_sent,
+      received: data.total_received,
+      recent_transactions: transactions
+    }
+    db.save(blockData);
+  });
+
+})
+
+app.get('/accounts', function(req, res) {
+
+  console.log('THIS IS A REQ BODY: ' + JSON.stringify(req.body));
+  db.Account.find({address: JSON.stringify(req.body.address)}).exec(function(err, account) {
+    if (err) {
+      console.log(err);
+    }
+    res.status(200).send(account);
+  });
+});
+
+app.listen(8080, function() {
+  console.log('listening on port 8080');
+});
+
+
+
