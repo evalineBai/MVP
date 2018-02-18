2f80f9d416b02f8cb622429c7b2ebcafa5ddbf56
Evaline <evalinebai@gmail.com>
2018-02-18 15:53:38 -0800

    Configure environment for styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
 var path = require('path');
+var webpack = require('webpack');
 
 var comp_dir = path.join(__dirname, '/client/index.js');
 var dist_dir = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: comp_dir,
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
   module: {
     loaders: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
       }
     ]
   },
