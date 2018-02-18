24c1ecd7ffb59196f56c6806be092a822fd24075
Evaline <evalinebai@gmail.com>
2018-02-18 12:13:42 -0800

    Add funny footer

---
+++ client/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <div>
+    <p>Thanks for visiting! If you enjoyed this product and wish to support the developer, all contributions can be made to 1Aks3fVX7rx1s6j3n19GgqMHiqRGdoV3YP.</p>
+
+  </div>
+)
+
+export default Footer;
